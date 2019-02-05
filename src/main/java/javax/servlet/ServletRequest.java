403371dd5f2b6a0af168c62898d8fd41498ef7df
Mark Thomas <markt@apache.org>
2019-02-05 11:11:15 +0000

    Update copyright year and format range as per Eclipse project handbook
    Signed-off-by: Mark Thomas <markt@apache.org>

--- src/main/java/javax/servlet/ServletRequest.java
+++ src/main/java/javax/servlet/ServletRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997-2018 Oracle and/or its affiliates and others.
+ * Copyright (c) 1997, 2019 Oracle and/or its affiliates and others.
  * All rights reserved.
  * Copyright 2004 The Apache Software Foundation
  *
