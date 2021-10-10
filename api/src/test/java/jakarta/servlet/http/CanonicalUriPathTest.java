35a24f2b99354b13b287e415add43f37eba82575
Greg Wilkins <gregw@webtide.com>
2021-10-10 16:50:26 +1100

    + simplified code + simplified examples + more edge cases

--- api/src/test/java/jakarta/servlet/http/CanonicalUriPathTest.java
+++ api/src/test/java/jakarta/servlet/http/CanonicalUriPathTest.java
@@ -4,18 +4,38 @@ import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Set;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CanonicalUriPathTest {
+    private static final Set<String> ENCODED_DOT_SEGMENT = Collections.unmodifiableSet(Set.of(
+            "%2e",
+            "%2E",
+            "%2e%2e",
+            "%2e%2E",
+            "%2E%2e",
+            "%2E%2E",
+            ".%2e",
+            ".%2E",
+            "%2e.",
+            "%2E."));
 
     public static String canonicalUriPath(String uriPath) {
         if (uriPath == null)
             throw new IllegalArgumentException("null path");
 
+        // Remember start/end conditions
         String path = uriPath;
+        boolean startsWithSlash = path.startsWith("/");
+        boolean endsWithSlash = path.endsWith("/");
+        boolean dotSegmentWithParam;
+        boolean encodedDotSegment;
+        boolean emptySegmentBeforeDotDot = false;
+        boolean decodeError = false;
 
         // Discard fragment.
         if (path.contains("#"))
@@ -25,74 +45,37 @@ public class CanonicalUriPathTest {
         if (path.contains("?"))
             path = path.substring(0, path.indexOf('?'));
 
-        // Remember start/end conditions
-        boolean startsWithSlash = path.startsWith("/");
-        boolean endsWithSlash = path.endsWith("/");
-        boolean dotSegmentWithParam = false;
-        boolean encodedDotSegment = false;
-        boolean emptySegmentBeforeDotDot = false;
-
         // Split path into segments.
-        List<String> segments = new ArrayList<>(Arrays.asList(path.substring(1).split("/")));
+        List<String> segments = new ArrayList<>(Arrays.asList(path.substring(startsWithSlash ? 1 : 0).split("/")));
 
         // Remove path parameters.
-        for (ListIterator<String> s = segments.listIterator(); s.hasNext();) {
-            String segment = s.next();
-            if (segment.contains(";")) {
-                segment = segment.substring(0, segment.indexOf(';'));
-                s.set(segment);
-
-                dotSegmentWithParam |= (".".equals(segment) || "..".equals(segment));
-            }
-        }
+        dotSegmentWithParam = segments.stream().anyMatch(s -> s.startsWith(".;") || s.startsWith("..;"));
+        segments.replaceAll(s -> (s.contains(";")) ? s.substring(0, s.indexOf(';')) : s);
 
         // Decode characters
-        for (ListIterator<String> s = segments.listIterator(); s.hasNext();) {
-            String segment = s.next();
-            if (segment.contains("%")) {
-                StringBuilder buf = new StringBuilder();
-                ByteArrayOutputStream utf8 = new ByteArrayOutputStream();
-                for (int i = 0; i < segment.length(); i++) {
-                    char c = segment.charAt(i);
-                    if (c == '%') {
-                        utf8.write(Integer.parseInt(segment.substring(i + 1, i + 3), 16));
-                        i += 2;
-                    } else {
-                        if (utf8.size() > 0) {
-                            buf.append(new String(utf8.toByteArray(), StandardCharsets.UTF_8));
-                            utf8.reset();
-                        }
-
-                        buf.append(c);
-                    }
-                }
-                if (utf8.size() > 0) {
-                    buf.append(new String(utf8.toByteArray(), StandardCharsets.UTF_8));
-                    utf8.reset();
-                }
-                segment = buf.toString();
-                s.set(segment);
-                encodedDotSegment |= (".".equals(segment) || "..".equals(segment));
-            }
+        encodedDotSegment = segments.stream().anyMatch(ENCODED_DOT_SEGMENT::contains);
+        try {
+            segments.replaceAll(CanonicalUriPathTest::decode);
+        } catch (Exception e) {
+            decodeError = true;
         }
 
         // Remove Empty Segments
-        segments.removeIf(s1 -> s1.length() == 0);
+        segments.removeIf(s -> s.length() == 0);
 
         // Remove dot-segments
         int count = 0;
         for (ListIterator<String> s = segments.listIterator(); s.hasNext();) {
             String segment = s.next();
-
             if (segment.equals(".")) {
                 s.remove();
             } else if (segment.equals("..")) {
                 if (count > 0) {
                     s.remove();
-                    String previous = s.previous();
+                    String prev = s.previous();
                     s.remove();
                     count--;
-                    emptySegmentBeforeDotDot |= previous.length() == 0;
+                    emptySegmentBeforeDotDot |= prev.length() == 0;
                 }
             } else {
                 count++;
@@ -101,118 +84,159 @@ public class CanonicalUriPathTest {
 
         // Concatenate segments
         StringBuilder buf = new StringBuilder();
-
-        for (ListIterator<String> s = segments.listIterator(); s.hasNext();) {
-            String segment = s.next();
-            if (s.hasPrevious() || startsWithSlash)
-                buf.append("/");
-            buf.append(segment);
-        }
+        segments.forEach(s -> buf.append("/").append(s));
         if (endsWithSlash)
             buf.append("/");
         path = buf.toString();
 
-        // Rejecting Suspicious Sequences
-        String reject = null;
+        // Rejecting Errors and Suspicious Sequences
+        String reject = "";
+        if (decodeError)
+            reject += "decode error; ";
         // Any path not starting with the `"/"` character
         if (!startsWithSlash)
-            reject = "must start with /";
+            reject += "must start with /; ";
         // Any path starting with an initial segment of `".."`
-        else if (segments.get(0).equals(".."))
-            reject = "leading dot-dot-segment";
+        if (!segments.isEmpty() && segments.get(0).equals(".."))
+            reject += "leading dot-dot-segment; ";
         // The encoded `"/"` character
-        else if (uriPath.contains("%2f") || uriPath.contains("%2F"))
-            reject = "encoded /";
+        if (uriPath.contains("%2f") || uriPath.contains("%2F"))
+            reject += "encoded /; ";
         // Any `"."` or `".."` segment that had a path parameter
-        else if (dotSegmentWithParam)
-            reject = "dot segment with parameter";
+        if (dotSegmentWithParam)
+            reject += "dot segment with parameter; ";
         // Any `"."` or `".."` segment with any encoded characters
-        else if (encodedDotSegment)
-            reject = "encoded dot segment";
+        if (encodedDotSegment)
+            reject += "encoded dot segment; ";
         // Any `".."` segment preceded by an empty segment
-        else if (emptySegmentBeforeDotDot)
-            reject = "empty segment before dot dot";
+        if (emptySegmentBeforeDotDot)
+            reject += "empty segment before dot dot; ";
         // Any empty segment with parameters
-        else if (uriPath.contains("/;"))
-            reject = "empty segment with parameters";
+        if (uriPath.contains("/;"))
+            reject += "empty segment with parameters; ";
         // The `"\"` character encoded or not.
-        else if (path.contains("\\"))
-            reject = "backslash character";
+        if (path.contains("\\"))
+            reject += "backslash character; ";
         // Any control characters either encoded or not.
-        else {
-            for (char c : path.toCharArray()) {
-                if (c < 0x20 || c == 0x7f) {
-                    reject = "control character";
-                    break;
-                }
+
+        for (char c : path.toCharArray()) {
+            if (c < 0x20 || c == 0x7f) {
+                reject = "control character; ";
+                break;
             }
         }
 
         System.err.printf("| `%s` | `%s` | %s %n",
                 uriPath,
                 path,
-                reject == null ? "" : ("400 " + reject));
+                reject.length() == 0 ? "" : ("400 " + reject.substring(0, reject.length() - 2)));
 
         return path;
     }
 
+    private static String decode(String segment) {
+        if (segment.contains("%")) {
+            StringBuilder buf = new StringBuilder();
+            ByteArrayOutputStream utf8 = new ByteArrayOutputStream();
+            for (int i = 0; i < segment.length(); i++) {
+                char c = segment.charAt(i);
+                if (c == '%') {
+                    int b = Integer.parseInt(segment.substring(i + 1, i + 3), 16);
+                    if (b < 0)
+                        throw new IllegalArgumentException("negative encoding");
+                    utf8.write(b);
+                    i += 2;
+                } else {
+                    if (utf8.size() > 0) {
+                        buf.append(new String(utf8.toByteArray(), StandardCharsets.UTF_8));
+                        utf8.reset();
+                    }
+
+                    buf.append(c);
+                }
+            }
+            if (utf8.size() > 0) {
+                buf.append(new String(utf8.toByteArray(), StandardCharsets.UTF_8));
+                utf8.reset();
+            }
+            segment = buf.toString();
+        }
+        return segment;
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {
-            "/public/file.txt",
-            "/public/dir/",
-            "/public//file.txt",
-            "/public/;/file.txt",
-            "/PUBLIC/file.txt",
-            "/public%2Ffile.txt",
-            "/public%5Cfile.txt",
-            "/public%00/file.txt",
-            "/public/./file.txt",
-            "/public/.;/file.txt",
-            "/public/%2e/file.txt",
-            "/public/%2e;/file.txt",
-            "/../docroot/public/file.txt",
-            "/public/dir/../file.txt",
-            "/public//../file.txt",
-            "/public/;param/../file.txt",
-            "/public/dir/..;/file.txt",
-            "/public/dir/%2e%2E/file.txt",
-            "/public/dir/.%2e/file.txt",
-            "/public/dir/%2E./file.txt",
-            "/public/dir/%2e%2e;/file.txt",
-            "/WEB-INF/web.xml",
-            "/web-inf/web.xml",
-            "/WEB-IN~1.DIR/web.xml",
-            "/WEB-INF;/web.xml",
-            "/WEB-INF%2Fweb.xml",
-            "/WEB-INF%5Cweb.xml",
-            "/WEB-INF%00/web.xml",
-            "/WEB-INF/./web.xml",
-            "/public/../WEB-INF/web.xml",
-            "/public/..;/WEB-INF/web.xml",
-            "/public/%2e%2e/WEB-INF/web.xml",
-            "/public/%2e%2e;/WEB-INF/web.xml",
-            "/secret/private.xml",
-            "/SeCreT/private.xml",
-            "/SECRET~1.DIR/private.xml",
-            "/secret;/private.xml",
-            "/secret%2Fprivate.xml",
-            "/secret%5Cprivate.xml",
-            "/secret%00/private.xml",
-            "/./secret/private.xml",
-            "/.;/secret/private.xml",
-            "/%2e/secret/private.xml",
-            "/%2e;/secret/private.xml",
-            "/public/../secret/private.xml",
-            "/public/..;/secret/private.xml",
-            "/public/%2e%2e/secret/private.xml",
-            "/public/%2e%2e;/secret/private.xml",
-            "/dispatch/public/file.txt",
-            "/dispatch/public%2Ffile.txt",
-            "/dispatch/public%5Cfile.txt",
-            "/dispatch/public%252Ffile.txt",
-            "/dispatch/WEB-INF/web.xml",
-            "/dispatch/secret/private.xml",
-            "/dispatch/%2E%2E/%2E%2E/etc/password",
+            "foo/bar",
+            "/foo/bar",
+            "/foo/bar/",
+            "/foo;/bar;",
+            "/foo;/bar;/;",
+            "/foo%00/bar/",
+            "/foo%7F/bar/",
+            "/foo%2Fbar",
+            "/foo\\bar",
+            "/foo%5Cbar",
+            "/foo;%2E/bar",
+            "/foo;%2F/bar",
+
+            "/foo/./bar",
+            "/foo/././bar",
+            "/./foo/bar",
+            "/foo/%2e/bar",
+            "/foo/.;/bar",
+            "/foo/%2e;/bar",
+            "/foo/.%2Fbar",
+            "/foo/.%5Cbar",
+            "/foo/bar/.",
+            "/foo/bar/./",
+            "/foo/bar/.;",
+            "/foo/bar/./;",
+            "/foo/.bar",
+
+            "/foo/../bar",
+            "/foo/../../bar",
+            "/../foo/bar",
+            "/foo/%2e%2E/bar",
+            "/foo/%2e%2e/%2E%2E/bar",
+            "/foo/..;/bar",
+            "/foo/%2e%2E;/bar",
+            "/foo/..%2Fbar",
+            "/foo/..%5Cbar",
+            "/foo/bar/..",
+            "/foo/bar/../",
+            "/foo/bar/..;",
+            "/foo/bar/../;",
+            "/foo/..bar",
+
+            "/foo/.../bar",
+
+            "/foo//bar",
+            "//foo//bar//",
+            "/;/foo;/;/bar/;/;",
+            "/foo//../bar",
+            "/foo/;/../bar",
+
+            "/foo%E2%82%ACbar",
+            "/foo%20bar",
+            "/foo%-1bar",
+            "/foo%XX/bar",
+            "/foo%/bar",
+            "/foo/bar%0",
+
+            "/",
+            "//",
+            "/;/",
+            "/.",
+            "/..",
+            "/./",
+            "/../",
+
+            "foo/bar/",
+            "./foo/bar/",
+            "%2e/foo/bar/",
+            "../foo/bar/",
+            ".%2e/foo/bar/",
+            ";/foo/bar/",
     })
 
     public void testCanonicalUriPath(String path) {
