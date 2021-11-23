052ad59a97f721d13b3a497e50eb67e914e22be6
Mark Thomas <markt@apache.org>
2021-11-23 07:43:26 +0000

    Add forRemoval and since information to deprecation annotations

--- api/src/main/java/jakarta/servlet/http/HttpServlet.java
+++ api/src/main/java/jakarta/servlet/http/HttpServlet.java
@@ -86,7 +86,7 @@ public abstract class HttpServlet extends GenericServlet {
      * @deprecated may be removed in future releases
      * @since 6.0
      */
-    @Deprecated
+    @Deprecated(forRemoval = true, since = "Servlet 6.0")
     public static final String LEGACY_DO_HEAD = "jakarta.servlet.http.legacyDoHead";
 
     private static final String LSTRING_FILE = "jakarta.servlet.http.LocalStrings";
@@ -621,7 +621,7 @@ public abstract class HttpServlet extends GenericServlet {
  * Response object.
  */
 // file private
-@Deprecated
+@Deprecated(forRemoval = true, since = "Servlet 6.0")
 class NoBodyResponse extends HttpServletResponseWrapper {
 
     private static final ResourceBundle lStrings = ResourceBundle.getBundle("jakarta.servlet.http.LocalStrings");
@@ -743,7 +743,7 @@ class NoBodyResponse extends HttpServletResponseWrapper {
  * Servlet output stream that gobbles up all its data.
  */
 // file private
-@Deprecated
+@Deprecated(forRemoval = true, since = "Servlet 6.0")
 class NoBodyOutputStream extends ServletOutputStream {
 
     private static final String LSTRING_FILE = "jakarta.servlet.http.LocalStrings";
