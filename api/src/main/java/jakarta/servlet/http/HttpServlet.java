b5286e5bf874c596dec8f4d83a72d680ac3e7e6e
Mark Thomas <markt@apache.org>
2023-03-22 15:33:40 +0000

    Minor improvements. Add @since. Improve wording.

--- api/src/main/java/jakarta/servlet/http/HttpServlet.java
+++ api/src/main/java/jakarta/servlet/http/HttpServlet.java
@@ -583,7 +583,7 @@ public abstract class HttpServlet extends GenericServlet {
      * {@code TRACE} request?
      *
      * <p>
-     * By default, the headers thats start with any of the following are considered sensitive:
+     * By default, headers with names that start with any of the following are considered sensitive:
      * <ul>
      * <li>authorization</li>
      * <li>cookie</li>
@@ -599,6 +599,8 @@ public abstract class HttpServlet extends GenericServlet {
      *
      * @return (@code true} if the HTTP request header is considered sensitive and should be excluded from the response to a
      * {@code TRACE} request, otherwise {@code false}
+     *
+     * @since Servlet 6.1
      */
     protected boolean isSensitiveHeader(String headerName) {
         String lcHeaderName = headerName.toLowerCase(Locale.ENGLISH);
