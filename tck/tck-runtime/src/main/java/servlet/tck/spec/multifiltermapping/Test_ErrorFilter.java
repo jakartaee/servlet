09ce2bd9bb33ef8396b76ee6c0da4bf694bd508b
Mark Thomas <markt@apache.org>
2025-06-13 12:23:41 +0100

    Clean-up

--- tck/tck-runtime/src/main/java/servlet/tck/spec/multifiltermapping/Test_ErrorFilter.java
+++ tck/tck-runtime/src/main/java/servlet/tck/spec/multifiltermapping/Test_ErrorFilter.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2007, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2025 Oracle and/or its affiliates and others.
+ * All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -13,11 +14,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
-
-/*
- * $Id$
- */
-
 package servlet.tck.spec.multifiltermapping;
 
 import java.io.IOException;
@@ -32,28 +28,19 @@ import jakarta.servlet.ServletResponse;
 
 public final class Test_ErrorFilter implements Filter {
 
-  // The filter configuration object we are associated with. If this value
-  // is null, this filter instance is not currently configured.
-  private FilterConfig filterConfig = null;
-
-  public void doFilter(ServletRequest request, ServletResponse response,
-      FilterChain chain) throws IOException, ServletException {
-    PrintWriter pw = response.getWriter();
-    pw.println("in Test_ErrorFilter");
-    chain.doFilter(request, response);
-  }
-
-  // Remove the filter configuration object for this filter.
-  public void destroy() {
-  }
-
-  // initialize the filter configuration object for this filter.
-  public void init(FilterConfig filterConfig) throws ServletException {
-    if (filterConfig == null) {
-      throw new ServletException(
-          "Failed to initialize the filter configuration");
-    } else {
-      this.filterConfig = filterConfig;
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        PrintWriter pw = response.getWriter();
+        pw.println("in Test_ErrorFilter");
+        chain.doFilter(request, response);
     }
-  }
+
+    // initialize the filter configuration object for this filter.
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        if (filterConfig == null) {
+            throw new ServletException("Failed to initialize the filter configuration");
+        }
+    }
 }
