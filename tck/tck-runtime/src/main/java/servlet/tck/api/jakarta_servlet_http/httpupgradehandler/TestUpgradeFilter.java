40ea7141c192168a5ac3f6d616b4bbcd117194e0
Matheus Oliveira <hi@omatheusmesmo.dev>
2026-02-01 16:50:22 -0300

    test: #900 - Create TestUpgradeFilter to set response headers
    Creates TestUpgradeFilter to set response headers proving filter invocation.
    This ensures that Filters are processed after HttpServletRequest.upgrade()
    is called.
    
    Fixes #900
    
    Signed-off-by: Matheus Oliveira <hi@omatheusmesmo.dev>

---
+++ tck/tck-runtime/src/main/java/servlet/tck/api/jakarta_servlet_http/httpupgradehandler/TestUpgradeFilter.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2025 Oracle and/or its affiliates. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception, which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ */
+
+package servlet.tck.api.jakarta_servlet_http.httpupgradehandler;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/TestServlet"})
+public class TestUpgradeFilter implements Filter {
+
+  private FilterConfig filterConfig = null;
+
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException{
+    if (filterConfig != null && response instanceof HttpServletResponse) {
+      HttpServletResponse httpResponse = (HttpServletResponse) response;
+      httpResponse.setHeader("X-Filter-Before-Upgrade", "invoked");
+    }
+
+    chain.doFilter(request, response);
+
+    if(filterConfig != null && response instanceof HttpServletResponse){
+      HttpServletResponse httpResponse = (HttpServletResponse) response;
+      httpResponse.setHeader("X-Filter-After-Upgrade", "processed");
+    }
+  }
+
+  public void destroy(){
+  }
+
+  public void init(FilterConfig filterConfig) throws ServletException{
+    this.filterConfig = filterConfig;
+  }
+}
